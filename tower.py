15c99a607ebf85e30cb6b944b2f265c2d8dc14b7
wavezzz <pharanyu.s@ku.th>
2017-10-03 14:15:29 +0700

    add box

--- tower.py
+++ tower.py
@@ -7,11 +7,14 @@ class TowerGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.background = arcade.load_texture('images/town.jpg')
+        self.box = arcade.Sprite('images/box1.png')
+        self.box.set_position(500, 350)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
-                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.box.draw()
+        
 
         
  
